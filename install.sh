95751cccae3b0021dd9831132307778740314254
Nathan Musoke <nathan.musoke@gmail.com>
2016-03-05 22:56:29 +1300

    Write a simple script to create symlinks in /home/nathan.
    Will write something more sophisticated later and eventually use something like ansible.

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ln -sfvb $(pwd)/.bashrc ~
+ln -sfvb $(pwd)/.gitconfig ~
+ln -sfvb $(pwd)/.gitignore ~
+ln -sfvb $(pwd)/.hgrc ~
+ln -sfvb $(pwd)/.i3/config ~
+ln -sfvb $(pwd)/.profile ~
+ln -sfvb $(pwd)/.taskrc ~
+ln -sfvb $(pwd)/.vim/ftplugin/tex.vim ~/.vim/ftplugin/tex.vim
+ln -sfvb $(pwd)/.vim/spell/en.latin1.add ~/.vim/spell/en.latin1.add
+ln -sfvb $(pwd)/.vim/spell/en.utf-8.add ~/.vim/spell/en.utf-8.add
+ln -sfvb $(pwd)/.vimrc ~
+ln -sfvb $(pwd)/git-completion.bash ~
