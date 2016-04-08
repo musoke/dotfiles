e016a5aee17ea6291ac39c7b250d43b3dccc7a51
Nathan Musoke <nathan.musoke@gmail.com>
2016-04-08 12:06:52 +1200

    Update install script to symlink directories
    symlink to directories instead of to the individual files within them.
    Also add *.spl to .gitignore as these will now be generated within
    local repositories.

--- install.sh
+++ install.sh
@@ -9,19 +9,16 @@ ln -sfvb $(pwd)/.gitconfig ~
 git config --global core.excludesfile $(echo ~/.gitignore_global)
 ln -sfvb $(pwd)/.gitignore_global ~
 ln -sfvb $(pwd)/git-completion.bash ~
+
 ln -sfvb $(pwd)/.hgrc ~
 
-ln -sfvb $(pwd)/.i3/config ~/.i3/
+ln -sfvb $(pwd)/.i3 ~/
 ln -sfvb $(pwd)/.taskrc ~
 
-ln -sfvb $(pwd)/.vim/ftplugin/tex.vim ~/.vim/ftplugin/tex.vim
-ln -sfvb $(pwd)/.vim/spell/en.latin1.add ~/.vim/spell/en.latin1.add
-ln -sfvb $(pwd)/.vim/spell/en.utf-8.add ~/.vim/spell/en.utf-8.add
 ln -sfvb $(pwd)/.vimrc ~
-ln -sfvb $(pwd)/.vim/bundle ~/.vim
-ln -sfvb $(pwd)/.vim/autoload ~/.vim
+ln -sfvb $(pwd)/.vim ~/
 
 ln -sfvb $(pwd)/.tmux.conf ~
 
-ln -sfvb $(pwd)/bin/apod.sh ~/bin/
-ln -sfvb $(pwd)/bin/os_release.sh ~/bin/
+ln -sfvb $(pwd)/bin ~/
+ln -sfvb $(pwd)/bin ~/
