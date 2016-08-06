d32edf473bbf5afa864b22a5347ff752091baea1
Nathan Musoke <nathan.musoke@gmail.com>
2016-08-06 22:52:09 +1200

    vimfx: disable some keys in Jupyter and gmail

---
+++ .config/vimfx/config.js
@@ -0,0 +1,20 @@
+// disable some commands in Jupyter notebooks
+vimfx.addKeyOverrides(
+  [ location => location.hostname === 'localhost' && 
+                location.port === '8888' && 
+                location.pathname.startsWith('/notebooks/'),
+    ['j', 'k', 
+     'r', 'm', 'y',
+     'f',
+     'a', 'b', 'x', 'c', 'v', 'z',
+     's', 'i', 'h',
+     'l', 'o']
+  ]
+)
+
+// disable some commands in gmail
+vimfx.addKeyOverrides(
+  [ location => location.hostname === 'mail.google.com',
+    ['j', 'k', 'r', 'a', '/']
+  ]
+)
