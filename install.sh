bce9588fdce7a89ce49bf987b9d257c822dc864c
Nathan Musoke <nathan.musoke@gmail.com>
2016-11-13 08:36:03 +1300

    Add git filters to clean passwords from some files
    .gitattributes says which filters and which files
    .gitconfig_local contains the filter definitions
    install.sh tells git to include .gitconfig_local in the local git config

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ ln -sfvb --suffix ~$(date +%Y-%m-%d-%H-%M-%S) $(pwd)/.bash_functions ~
 ln -sfvb --suffix ~$(date +%Y-%m-%d-%H-%M-%S) $(pwd)/.bashrc_ubuntu ~
 ln -sfvb --suffix ~$(date +%Y-%m-%d-%H-%M-%S) $(pwd)/.profile ~
 
+git config --local include.path ../.gitconfig_local
 ln -sfvb --suffix ~$(date +%Y-%m-%d-%H-%M-%S) $(pwd)/.gitconfig ~
 ln -sfvb --suffix ~$(date +%Y-%m-%d-%H-%M-%S) $(pwd)/.gitignore_global ~
 ln -sfvb --suffix ~$(date +%Y-%m-%d-%H-%M-%S) $(pwd)/git-completion.bash ~
