95a00dd88defc7644b96c48f8f6428e096760e37
Nathan Musoke <nathan.musoke@gmail.com>
2021-07-01 13:24:19 -0300

    borg: Add more exclusions to backups

--- bin/borg-auto.sh
+++ bin/borg-auto.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# REPOSITORY=/media/VERBATIM\ HD/borg-repo
-REPOSITORY=/media/Elements/borg-repo
+# REPOSITORY=/media/nathan/VERBATIM\ HD/borg-repo
+# REPOSITORY=/media/nathan/Elements/borg-repo
+REPOSITORY=/media/nathan/Elements\ SE/borg-repo
 
 echo "Using repository: \"$REPOSITORY\""
 
@@ -9,6 +10,7 @@ borg create -v --stats --progress                       \
     --compression zlib,6                                \
     "$REPOSITORY"::'{hostname}-{now:%Y-%m-%d-%H-%M-%S}' \
     /home                                               \
+    --exclude-caches                                    \
     --exclude '/home/*/.ssh'                            \
     --exclude '/home/*/.gnupg/random_seed'              \
     --exclude '/home/*/.gnupg/.#*'                      \
@@ -16,6 +18,7 @@ borg create -v --stats --progress                       \
     --exclude '/home/*/.mutt/temp'                      \
     --exclude '/home/*/.mpd/pid'                        \
     --exclude '/home/*/.mpd/database'                   \
+    --exclude '/home/*/.mpd/log'                        \
     --exclude '/home/*/.mpd/state'                      \
     --exclude '/home/*/.mozilla/firefox/*/Cache'        \
     --exclude '/home/*/.config/chromium'                \
@@ -25,6 +28,11 @@ borg create -v --stats --progress                       \
     --exclude '/home/*/.dropbox-dist'                   \
     --exclude '/home/*/.conda'                          \
     --exclude '/home/*/.cargo'                          \
+    --exclude '/home/*/target/debug'                    \
+    --exclude '/home/*/target/rls'                      \
+    --exclude '/home/*/.julia/artifacts'                \
+    --exclude '/home/*/.julia/compiled'                 \
+    --exclude '/home/*/.julia/packages'                 \
     --exclude '/home/*/.rustup'                         \
     --exclude '/home/*/go'                              \
     --exclude '/home/*/.theano'                         \
@@ -34,15 +42,21 @@ borg create -v --stats --progress                       \
     --exclude '/home/*/.recently-used.xbel'             \
     --exclude '/home/*/.thumbnails'                     \
     --exclude '/home/*/.Xauthority'                     \
+    --exclude '/home/*/.local/lib/python*'              \
     --exclude '/home/*/.local/share/Trash'              \
     --exclude '/home/*/.local/share/flatpak'            \
-    --exclude '/home/*/.AndroidStudio*'            \
-    --exclude '/home/*/snap'
+    --exclude '/home/*/.local/share/marble'             \
+    --exclude '/home/*/.AndroidStudio*'                 \
+    --exclude '/home/*/.Android*'                       \
+    --exclude '/home/*/anaconda*'                       \
+    --exclude '/home/*/snap'                            \
+    --exclude '/home/*/media/videos/movies'
 
 
 # Prune archives from this machine.
 borg prune -v --stats --list                            \
     "$REPOSITORY" --prefix '{hostname}-'                \
+    --keep-within=2d                                    \
     --keep-daily=7                                      \
     --keep-weekly=8                                     \
     --keep-monthly=12                                   \
