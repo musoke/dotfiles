408503e8ca8b8b71f0e71ece8cc5a313b90dbb1a
Nathan Musoke <nathan.musoke@gmail.com>
2023-05-11 18:26:22 -0400

    julia: PkgTemplate.Template in startup.jl

--- julia/.julia/config/startup.jl
+++ julia/.julia/config/startup.jl
@@ -1,2 +1,24 @@
 using OhMyREPL
 using Revise
+
+function template()
+    @eval begin
+        using PkgTemplates
+        Template(
+            user = "musoke",
+            plugins = [
+                Tests(project=true),
+                GitHubActions(extra_versions=["1.8", "nightly"]),
+                GitHubPages(),
+                Codecov(),
+                Documenter{GitHubActions}(),
+                Git(ssh = true),
+                TravisCI(),
+                Dependabot(),
+                Develop(),
+                Citation(readme=true),
+                RegisterAction(),
+            ],
+        )
+    end
+end
